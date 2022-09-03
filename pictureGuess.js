5e647dee3d75c8f205fcaacddf1138fedc549448
gmddurachok <99399973+gmddurachok@users.noreply.github.com>
2022-09-03 17:11:08 +0300

    Update pictureGuess.js

--- pictureGuess.js
+++ pictureGuess.js
@@ -1,56 +1,135 @@
 let lineValue = document.getElementById("levelInput").value;
-img = document.getElementById("levelImage");
-let count = 0;
+let img = document.getElementById("levelImage");
+let count = 0.0;
+let hardest = 'easy';
+let difficulity = document.getElementById("diff");
 let rnd = 0;
-
-function randomize()
- {
-     rnd = Math.floor(Math.random() * levelNames.length);
- }
-randomize();
+document.getElementsByTagName('img').ondragstart = function() { return false; };
 function next()
- {
-    document.getElementById("levelInput").placeholder = "Правильных ответов: "+count;
-    randomize();
-    img.src = dir+rnd+".jpg";
- 	document.getElementById('background').style.background = "url("+dir+rnd+".jpg)";
- }
-img.src = dir+rnd+".jpg";
-document.getElementById('background').style.background = "url("+dir+rnd+".jpg)";
- 
-function check()
- {
-     lineValue = document.getElementById("levelInput").value;
-     if(lineValue.toLowerCase() == levelNames[rnd])
-     {
-         document.getElementById("levelInput").value = "";
-         count+=1;
-         console.log("Пользователь угадал уровень. +1, счёт "+count);
-         next();
-     }
-     else 
-     {
-         alert("Неправильно");
-     }
-     
- }
-function skip()
- {
-     if(count == 0)
-     {
-         alert("Пропускать уровень с 0 очками нельзя");
-     }
-     else
-     {
-         document.getElementById("levelInput").value = "";
-         count = count-1;
-         alert("Это "+levelNames[rnd])
-         console.log("Пользователь пропустил уровень, -1, счёт: "+count)
-         next();
-     }
- }
-document.getElementById("levelInput").addEventListener('keydown', function(e) {
-    if (e.key == "Enter") {
-      check()
+{
+    switch(hardest)
+    {
+        case "easy":
+            rnd = getRandomInt(0,easyar.length-1);
+            img.src = dir+"easy/"+rnd+fmt;
+            document.getElementById("levelInput").value = "";
+            console.log("Легко, "+easyar[rnd]);
+            break;
+        case "hard":
+            rnd = getRandomInt(0,hardar.length-1);
+            document.getElementById("levelInput").value = "";
+            img.src = dir+"hard/"+rnd+fmt;
+            console.log("Сложно, "+hardar[rnd]);
+            break;
+        case "impossible":
+            rnd = getRandomInt(0,impossiblear.length-1);
+            document.getElementById("levelInput").value = "";
+            img.src = dir+"impossible/"+rnd+fmt;
+            console.log("Невозможно, "+impossiblear[rnd]);
+            break;
     }
-  });
+}
+next();
+function check()
+{
+    switch(hardest)
+    {
+        case "easy":
+            if(lineValue == easyar[rnd])
+            {
+                count = count+0.5;
+                lineValue = "";
+                document.getElementById("levelInput").placeholder = count;
+                next();
+            }
+            else
+            {
+                alert("Неправильно");
+            }
+            break;
+        case "hard":
+            if(lineValue == hardar[rnd])
+            {
+                count = count+1;
+                lineValue = "";
+                document.getElementById("levelInput").placeholder = count;
+                next();
+            }
+            else
+            {
+                alert("Неправильно");
+            }
+            break;
+        case "impossible":
+            if(lineValue == impossiblear[rnd])
+            {
+                count = count+3;
+                lineValue = "";
+                document.getElementById("levelInput").placeholder = count;
+                next();
+            }
+            else
+            {
+                alert("Неправильно");
+            }
+            break;
+    }
+}
+document.getElementById("levelInput").addEventListener('keypress', function (e)
+{
+    //SHITCODE
+    if(e.key == "Enter")
+    {
+        lineValue = document.getElementById("levelInput").value;
+        check()
+    }
+})
+difficulity.addEventListener('change', function (e) {
+    switch(e.target.value)
+    {
+        case "easy":
+            hardest = "easy";
+            next();
+            break;
+        case "hard":
+            hardest = "hard";
+            next();
+            break;
+        case "impossible":
+
+            hardest = "impossible";
+            next();
+            break;
+    }
+  })
+function skip()
+{
+    if(count <= 0)
+    {
+        alert("0 очков.")
+    }
+    else
+    {
+        switch(hardest)
+        {
+            case "easy":
+                alert("Ответ: "+ easyar[rnd]);
+                count = count - 0.5;
+                document.getElementById("levelInput").placeholder = count;
+                next();
+                break;
+            case "hard":
+                alert("Ответ: "+ hardar[rnd]);
+                count = count - 1;
+                document.getElementById("levelInput").placeholder = count;
+                next();
+                break;
+            case "impossible":
+                alert("Ответ: "+ impossiblear[rnd]);
+                count = count - 1;
+                document.getElementById("levelInput").placeholder = count;
+                next();
+                break;
+        }
+    }
+}
