dc7dfdc96b0b247c557c3257a3ab081ee742be07
gmddurachok <99399973+gmddurachok@users.noreply.github.com>
2022-09-03 17:11:25 +0300

    Update nameGuess.js

--- nameGuess.js
+++ nameGuess.js
@@ -1,55 +1,48 @@
-counter = document.getElementById('counter');
 count = 10;
 let txt = document.getElementById('whatguess');
 let rnd1, rnd2, rnd3, rnd4, correctImg;
     function next()
     {
-        rnd1 = getRandomInt(0, levelNames.length-1);
-        rnd2 = getRandomInt(0, levelNames.length-1);
-        rnd3 = getRandomInt(0, levelNames.length-1);
-        rnd4 = getRandomInt(0, levelNames.length-1);
+        rnd1 = getRandomInt(0, easyar.length-2);
+        rnd2 = getRandomInt(0, easyar.length-2);
+        rnd3 = getRandomInt(0, easyar.length-2);
+        rnd4 = getRandomInt(0, easyar.length-2);
         correctImg = getRandomInt(1, 4);
         document.getElementById('img1').style.opacity = 100;
         document.getElementById('img2').style.opacity = 100;
         document.getElementById('img3').style.opacity = 100;
         document.getElementById('img4').style.opacity = 100;
         console.log(correctImg)
-        counter.innerHTML = count;
-        document.getElementById('background').style.background = "url("+dir+rnd3+".jpg)";
-        if(count <= 0)
-        {
-            document.getElementById("background").filter = "blur(30px); ";
-        }
         if(rnd1 == rnd2 || rnd1 == rnd3 || rnd1 == rnd4 || rnd2 == rnd1 || rnd2 == rnd3 || rnd2 == rnd4 || rnd3 == rnd1 || rnd3 == rnd2 || rnd3 == rnd4 || rnd4 == rnd1 || rnd4 == rnd2 || rnd4 == rnd3)
         {
             next()
         }
-        txt.innerHTML = levelNames[rnd1];
+        txt.innerHTML = easyar[rnd1]+" | "+count;
     if(correctImg == 1)
     {
-        document.getElementById('img2').src = dir+rnd2+".jpg";
-        document.getElementById('img3').src = dir+rnd3+".jpg";
-        document.getElementById('img4').src = dir+rnd4+".jpg";
+        document.getElementById('img2').src = dir+"easy/"+rnd2+".jpg";
+        document.getElementById('img3').src = dir+"easy/"+rnd3+".jpg";
+        document.getElementById('img4').src = dir+"easy/"+rnd4+".jpg";
     }
     if(correctImg == 2)
     {
-        document.getElementById('img1').src = dir+rnd2+".jpg";
-        document.getElementById('img3').src = dir+rnd3+".jpg";
-        document.getElementById('img4').src = dir+rnd4+".jpg";
+        document.getElementById('img1').src = dir+"easy/"+rnd2+".jpg";
+        document.getElementById('img3').src = dir+"easy/"+rnd3+".jpg";
+        document.getElementById('img4').src = dir+"easy/"+rnd4+".jpg";
     }
     if(correctImg == 3)
     {
-        document.getElementById('img1').src = dir+rnd2+".jpg";
-        document.getElementById('img2').src = dir+rnd3+".jpg";
-        document.getElementById('img4').src = dir+rnd4+".jpg";
+        document.getElementById('img1').src = dir+"easy/"+rnd2+".jpg";
+        document.getElementById('img2').src = dir+"easy/"+rnd3+".jpg";
+        document.getElementById('img4').src = dir+"easy/"+rnd4+".jpg";
     }
     if(correctImg == 4)
     {
-        document.getElementById('img1').src = dir+rnd2+".jpg";
-        document.getElementById('img2').src = dir+rnd3+".jpg";
-        document.getElementById('img3').src = dir+rnd4+".jpg";
+        document.getElementById('img1').src = dir+"easy/"+rnd2+".jpg";
+        document.getElementById('img2').src = dir+"easy/"+rnd3+".jpg";
+        document.getElementById('img3').src = dir+"easy/"+rnd4+".jpg";
     }
-    document.getElementById('img'+correctImg).src = dir+rnd1+".jpg";
+    document.getElementById('img'+correctImg).src = dir+"easy/"+rnd1+".jpg";
     
     }
     next();
@@ -58,13 +51,13 @@ function checkVariant(variant)
     if(variant == correctImg)
     {
         count=count+1;
+        txt.innerHTML = easyar[rnd1]+" | "+count;
         next();
     }
     else 
     {
         count=count-1;
-        counter.innerHTML = count;
-        alert("Неправильно")
+        txt.innerHTML = easyar[rnd1]+" | "+count;
         document.getElementById('img'+variant).style.opacity = 0;
     }
 }
