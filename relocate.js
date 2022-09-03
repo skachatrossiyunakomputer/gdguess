8bc250c187420122bc58ce698953d1730ef1fa96
gmddurachok <99399973+gmddurachok@users.noreply.github.com>
2022-09-03 17:10:45 +0300

    Update relocate.js

--- relocate.js
+++ relocate.js
@@ -1,16 +1,19 @@
-function relocate()
-{
-   let select = document.getElementById("mode").value;
-   if (select == "pictureGuess") {
-       window.location.replace("index.html");
-   }
-   if (select == "nameGuess") {
-       window.location.replace("pict.html");
-   }
-   if (select == "musicGuess") {
-       window.location.replace("music.html");
-   }
-   if (select == "selectMode") {
-    window.location.replace("https://youtu.be/dQw4w9WgXcQ");
-}
-}
+frame = document.getElementById("fram")
+let mode = document.getElementById("mode");
+mode.addEventListener('change', function(e) {
+        switch(e.target.value)
+        {
+            case "musicGuess":
+                frame.src = "music.html";
+                break;
+            case "nameGuess":
+                frame.src = "pict.html";
+                break;
+            case "pictureGuess":
+                frame.src = "name.html";
+                break;
+            case "selectMode":
+                frame.src = "https://youtu.be/dQw4w9WgXcQ";
+                break;
+        }
+});
