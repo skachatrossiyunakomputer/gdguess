a2a21c22a9d9210886e05d068d2d247d5fb9b8f2
gmddurachok <99399973+gmddurachok@users.noreply.github.com>
2022-09-03 17:10:23 +0300

    Update music.js

--- music.js
+++ music.js
@@ -1,18 +1,24 @@
-songer = document.getElementById('songs')
-lineText = document.getElementById('songInput').value;
+let songer = document.getElementById('songs')
+let lineText = document.getElementById('songInput').value;
+let playing = false;
+songer.volume -= 0.7;   
 let songs = [
     "stereo madness",
     "deadlocked",
-    "ludicrous speed",
-    "sonic blaster",
+    "supersonic",
+    "sonic wave",
     "trip"
 ]
 let rnd = 0;
 function randomize()
 {
+    document.getElementById('songInput').value = "";
     rnd = getRandomInt(1, songs.length-1);
     console.log(songs[rnd] + " songs/"+rnd+".mp3");
-    songer.src = "songs/"+rnd+".mp3"
+    songer.src = "songs/"+rnd+".mp3";
+    songer.play();
+    document.getElementById('playbut').innerHTML = "❚❚";
+    playing = true;
 }
 function check()
 {
@@ -22,6 +28,25 @@ function check()
         randomize();
    }
 }
+function play()
+{
+    if(playing == false)
+    {
+        songer.play();
+        document.getElementById('playbut').innerHTML = "❚❚";
+        playing = true;
+    }
+    else
+    {
+        songer.pause();
+        document.getElementById('playbut').innerHTML = "▶";
+        playing = false; //❚❚
+    }
+}
+songer.addEventListener("ended", function(e) {
+    document.getElementById('playbut').innerHTML = "▶";
+    playing = false; //❚❚
+})
 randomize();
 document.getElementById("songInput").addEventListener('keydown', function(e) {
     if (e.key == "Enter") {
